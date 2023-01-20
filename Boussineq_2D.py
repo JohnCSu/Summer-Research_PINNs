c4f32f2e084aa6890bbf43d5f02c488746a219e3
John Su <johnchangsu@gmail.com>
2023-01-20 16:15:59 +1100

    Need To Fix Node to Data Mapping

--- Boussineq_2D.py
+++ Boussineq_2D.py
@@ -19,7 +19,7 @@ def aux_wrap(net):
         result = net(xyt)
         return result,result
     return aux_net
-def get_conc_derivs(net,xyt,hes_vecs):
+def get_boussineq_derivs(net,xyt,hes_vecs):
     '''
     Get the all the derivatives for the equations for Navier Stokes and Concentration Equations
     Assumes the NN Model maps: (x,y,t) --> (u,v,p,c)
@@ -55,7 +55,10 @@ def get_conc_derivs(net,xyt,hes_vecs):
     return (p,px,py), (temp,temp_x,temp_y,temp_t,temp_xx,temp_yy),(u,ux,uy,ut,uxx,uyy),(v,vx,vy,vt,vxx,vyy) 
     
 # Code for one epoch
-def one_epoch(train_loader,net,optimizer,Re = 100, bouy = 100,alpha = 100, device = 'cpu',weights = None):
+def one_epoch(train_loader,net,optimizer,params, device = 'cpu',weights = None):
+    '''
+    Dimensional Training Loop
+    '''
     vecs = torch.zeros((6,4,3))
     #These tensors allow us to effeciently get 2nd order derivatives without
     #Computing the entire Hessian
@@ -79,21 +82,24 @@ def one_epoch(train_loader,net,optimizer,Re = 100, bouy = 100,alpha = 100, devic
     running_loss = 0.
     indi_loss = torch.zeros(6)
 
+
+    rho,mu,g,beta,alpha = params
     for data in train_loader:
         optimizer.zero_grad()
         
         xyt,u_data,v_data = data[0].to(device),data[1].to(device),data[2].to(device)
-        (p,px,py), (temp,temp_x,temp_y,temp_t,temp_xx,temp_yy),(u,ux,uy,ut,uxx,uyy),(v,vx,vy,vt,vxx,vyy) = vmap(get_conc_derivs,(None,0,None))(net,xyt,vecs)
+        (p,px,py), (temp,temp_x,temp_y,temp_t,temp_xx,temp_yy),(u,ux,uy,ut,uxx,uyy),(v,vx,vy,vt,vxx,vyy) = vmap(get_boussineq_derivs,(None,0,None))(net,xyt,vecs)
         #we output u,y,T-T0 as T-T0 scales to -1 and 1. We can recover original temp by adding T0
 
         #Boussineq stokes eq
-        e1 = ut + u*ux+v*uy + px - 1/Re*(uxx + uyy) 
+        e1 = ut + u*ux+v*uy + 1/rho*px - mu/rho*(uxx + uyy) 
         #y direction has additional buoyancy term
-        e2 = vt + vx*u +vy*v + py - 1/Re*(vxx+vyy) + bouy*(temp)
+        e2 = vt + vx*u +vy*v + 1/rho*py - mu/rho*(vxx+vyy)  + g*beta*(temp)
         #Incompress
         e3 = ux+vy
         #Concentration eq
         e4 = temp_t + (u*temp_x + v*temp_y) - alpha*(temp_xx+temp_yy)
+        # e4 = tensor([0.])
         # Data Fitting
         e5 = u-u_data 
         e6 = v-v_data
@@ -110,6 +116,75 @@ def one_epoch(train_loader,net,optimizer,Re = 100, bouy = 100,alpha = 100, devic
         #Loss Tracking
         running_loss += total_loss
         indi_loss += tensor([e.pow(2).mean() for e in eq_s])
+        
+    return running_loss ,indi_loss 
+
+
+def one_epoch2(train_loader,net,optimizer,params, device = 'cpu',weights = None):
+    '''
+    Non-Dimensional Form Training Loop
+    '''
+    vecs = torch.zeros((6,4,3))
+    #These tensors allow us to effeciently get 2nd order derivatives without
+    #Computing the entire Hessian
+    #uxx uyy
+    vecs[0,0,0] = 1
+    vecs[1,0,1] = 1
+
+    #vxx vyy
+    vecs[2,1,0] = 1
+    vecs[3,1,1] = 1
+
+    #Cxx, Cyy
+    vecs[4,3,0] = 1
+    vecs[5,3,1] = 1
+    vecs = vecs.to(device)
+
+
+    if weights is None:
+        weights = [1.]*6
+    #Variables to keep track of total loss and individual losses (unweighted)
+    running_loss = 0.
+    indi_loss = torch.zeros(6)
+
+
+    Re,bouy,alpha  = params
+    for data in train_loader:
+        optimizer.zero_grad()
+        
+        xyt,u_data,v_data = data[0].to(device),data[1].to(device),data[2].to(device)
+        # (p,px,py), (temp,temp_x,temp_y,temp_t,temp_xx,temp_yy),(u,ux,uy,ut,uxx,uyy),(v,vx,vy,vt,vxx,vyy) = vmap(get_boussineq_derivs,(None,0,None))(net,xyt,vecs)
+        # #we output u,y,T-T0 as T-T0 scales to -1 and 1. We can recover original temp by adding T0
+
+        # #Boussineq stokes eq
+        # e1 = ut + u*ux+v*uy + px - 1/Re*(uxx + uyy) 
+        # #y direction has additional buoyancy term
+        # e2 = vt + vx*u +vy*v + py - 1/Re*(vxx+vyy)  + bouy*(temp)
+        # #Incompress
+        # e3 = ux+vy
+        # #Concentration eq
+        # e4 = temp_t + (u*temp_x + v*temp_y) - alpha*(temp_xx+temp_yy)
+        # # e4 = tensor([0.])
+        # # Data Fitting
+        
+        out = net(xyt)
+        up = out[:,0]
+        vp = out[:,1]
+        e5 = up-u_data 
+        e6 = vp-v_data
+        # eq_s = [e1,e2,e3,e4,e5,e6]
+        eq_s = [e5,e6]
+        #Sum of MSE of each loss
+        
+        # total_loss = sum([w*e.pow(2).mean() for w,e in zip(weights,eq_s)])
+        total_loss = e5.pow(2).mean()+e6.pow(2).mean()
+        #Get Gradients then update weights
+        total_loss.backward()
+        optimizer.step()
+        
+        #Loss Tracking
+        running_loss += total_loss
+        # indi_loss += tensor([e.pow(2).mean() for e in eq_s])
         
     return running_loss ,indi_loss 
 
@@ -145,18 +220,19 @@ def main(MAX_EPOCHS = 10_000,N_SAMPLE = None,batch_size = 10_000,MAX_HOUR_DURATI
         print(f'Folder {SAVE_PATH} Created')
 
     #Set Network Up
-    net = HFM_Net(3,4,10,200)
+    net = HFM_Net(3,4,20,30)
     device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
     # net.non_lin = torch.tanh
+
     net = net.to(device)
 
     #Data Wrangling see HFM_training data for how data works (from HFM paper)
-    x_data,y_data,t_data,u_data,v_data,temp_data,p_data = np.moveaxis(get_boussineq_2D('Data/DHC_2D_Unsteady.dat'),0,-1)
+    x_data,y_data,t_data,u_data,v_data,temp_data,p_data = np.moveaxis(get_boussineq_2D('Data/DHC_2D_Unsteady.dat',shuffle=True),0,-1)
     
     #Dimensional Scales
     RHO = 998
-    L = 0.1
-    U = 1e-3
+    L =  0.1
+    U =  1e-4
     MU = 0.00959
     #For Temp inthis case leave as 1 
     T = 1 
@@ -164,13 +240,10 @@ def main(MAX_EPOCHS = 10_000,N_SAMPLE = None,batch_size = 10_000,MAX_HOUR_DURATI
     BETA = 0.000228
     #Conductivity #specific Heat
     k,Cp = 0.606,4181
-    alpha_star = k/(Cp*RHO)
+    alpha = k/(Cp*RHO)
 
-    Re = RHO*U*L/MU
-    bouy = -9.81*L*BETA/(U**2)
-    alpha = alpha_star/(U*L)
-
-    # Rescale All values
+    params = [RHO,MU,-9.81,BETA,alpha]
+    #Rescale All values
     x_data = x_data/L
     y_data = y_data/L
     t_data = t_data/(L/U)
@@ -180,16 +253,26 @@ def main(MAX_EPOCHS = 10_000,N_SAMPLE = None,batch_size = 10_000,MAX_HOUR_DURATI
     p_data = p_data/(RHO*U**2)
     temp_data = temp_data/T
 
-    
+    Re = RHO*U*L/MU
+    bouy = -9.81*L*BETA/(U**2)
+    Pr = (MU/RHO)/alpha
+
+    params = [Re,bouy,1/(Re*Pr)]
+    print(f'Dimensional Numbers: Re {Re:.3E}, Buoy {bouy:.3E}, 1/(Pr*Re) {params[-1]:.3E}')
+
+
     data = np.stack([x_data,y_data,t_data,u_data,v_data], axis = 1)
     new_data = tensor(data,dtype = torch.float32).squeeze()
     #DataLoader for Easy shuffling of data. Only Use 2_000_000 points
     if N_SAMPLE is None:
         N_SAMPLE = new_data.shape[0]
     
-    train_set = Boussineq_Dataset(new_data[:N_SAMPLE,:])
-    train_loader = DataLoader(train_set,batch_size = batch_size,shuffle = True,num_workers =0)
-    # a = iter(train_loader)
+    #Dummy Data
+    # x_t,y_t,t_t = torch.arange(0,10,1000),torch.arange(0,10,1000),torch.arange(0,10,1000)
+    # new_data = torch.stack([x_t,y_t,t_t,25*torch.sin(y_t*x_t),25*torch.cos(y_t*x_t)],dim = -1)
+
+    train_set = Boussineq_Dataset(new_data[:N_SAMPLE])
+    train_loader = DataLoader(train_set,batch_size = batch_size,shuffle = True,num_workers =0,pin_memory=True)
 
 
     optimizer = torch.optim.Adam(net.parameters(),lr = 1e-3)
@@ -201,7 +284,7 @@ def main(MAX_EPOCHS = 10_000,N_SAMPLE = None,batch_size = 10_000,MAX_HOUR_DURATI
 
 
     print(f'\n\n Starting Run at time {currentDateAndTime} using device {device} \n\n')
-    print(f'Dimensional Numbers: Re {Re:.3f}, Buoy {bouy:.3f}, alpha {alpha:.7f}')
+    
 
     with open(f'{SAVE_PATH}/Run_{currentDateAndTime}.csv','w',newline = '') as f:
 
@@ -210,16 +293,20 @@ def main(MAX_EPOCHS = 10_000,N_SAMPLE = None,batch_size = 10_000,MAX_HOUR_DURATI
         csv_writer.writerow(['Epoch','Total Loss','NS_u','NS_y','Incompressibility','Conc_eq','Conc_data'])
 
         timer_start = time.perf_counter()
+        # weights = [ 1.E3,1.E3,1.E3,1.E3,1.E3,1.E3]
+        weights = [0,0,0,0,1,0]
+        # weights = None
         for i in range(MAX_EPOCHS):
             #One epoch training
-            loss,indi_loss = one_epoch(train_loader,net,optimizer,Re = Re, bouy = bouy, alpha = alpha, device = device)
+            loss,indi_loss = one_epoch2(train_loader,net,optimizer,params, device = device,weights = weights)
+            
             #Record Losses and elapsed time
             epoch_losses.append( [i,float(loss)] + list(map(float,indi_loss)) )
             csv_writer.writerow(epoch_losses[-1])
             elapsed_time = (time.perf_counter() - timer_start)/3600 
             
             if i % 2  == 0:
-                print(f'Time (H) {elapsed_time:.3f} Epoch {i} loss {loss:3f} Equation Losses \t {indi_loss}') 
+                print(f'Time (H) {elapsed_time:.3f} Epoch {i} loss {loss:.3E} Equation Losses \t {indi_loss}') 
 
                 if loss < best_loss:
                     torch.save(net.to('cpu').state_dict(),f'{SAVE_PATH}/Best_Loss_run_{currentDateAndTime}.pth')
@@ -231,7 +318,7 @@ def main(MAX_EPOCHS = 10_000,N_SAMPLE = None,batch_size = 10_000,MAX_HOUR_DURATI
 
 
 if __name__ == '__main__':
-    main(MAX_EPOCHS=10_000,N_SAMPLE=3_000_000,batch_size=10_000)
+    main(MAX_EPOCHS=5500,N_SAMPLE=5024,batch_size=1024)
 
 
     
--- Data/Boussineq_2D_Data.py
+++ Data/Boussineq_2D_Data.py
@@ -1,28 +1,30 @@
 import numpy as np
 import pickle
 
-def get_boussineq_2D(file_path:str):
+def get_boussineq_2D(file_path:str,shuffle=True):
     with open(file_path,'rb') as f:
         data = pickle.load(f)
 
     data.keys()
 
     coords = data['coords']
-    x_pre ,y_pre = coords[:,0], coords[:,-1] # Nx1
-    t_pre = data['time'] #Tx1
+    x_pre ,y_pre = coords[:,0][:,np.newaxis] , coords[:,-1][:,np.newaxis] # Nx1
+    t_pre = data['time'][:,np.newaxis] #Tx1
     
     #Reshape coords and t to be NxT then flatten
     # x,y are Nx1 and t is Tx1
-    x = np.stack([x_pre]*len(t_pre),axis = -1)
-    y = np.stack([y_pre]*len(t_pre),axis = -1)
-    t = np.stack([t_pre]*len(x_pre),axis = 0)
+    T = t_pre.shape[0]
+    N = x_pre.shape[0]
 
+    x= np.tile(x_pre,(1,T))
+    y= np.tile(y_pre,(1,T))
+    t= np.tile(t_pre,(1,N)).T
     #Field Data is in shape NxT already
     u = data['u_velocity']
     v = data['y_velocity']
     temp = data['temperature']
     p = data['pressure']
-
+    # return x_pre,y_pre,t_pre,u,v,temp
     # Flatten data into one long as array
     x_data = x.flatten()
     y_data = y.flatten()
@@ -34,9 +36,40 @@ def get_boussineq_2D(file_path:str):
     p_data = p.flatten()
     
     data = np.stack([x_data,y_data,t_data,u_data,v_data,temp_data,p_data],axis = -1)
-    np.random.seed(42)
-    np.random.shuffle(data)
+    if shuffle:
+        np.random.seed(42)
+        np.random.shuffle(data)
     # raise ValueError('OI SHUFFLE THE DATA YOU MONG')
     # Put all arrays together into one array
     return data
     # return x_data,y_data,t_data,u_data,v_data,temp_data,p_data
+
+if __name__ == '__main__':
+    from matplotlib import pyplot as plt
+    # x,y,t,u,v,temp = get_boussineq_2D('Data/DHC_2D_Unsteady.dat',shuffle=False)
+
+
+    # for i in range(0,40401,1000):
+    #     plt.plot(t,v[i])
+    #     plt.title('v velocitys of 1000 nodes over time')
+    # plt.show()
+    # print(u[:,0].shape)
+
+    # v_mag = np.sqrt(v[:,-1]**2 + u[:,-1]**2)
+    # cs = plt.tricontourf(x.squeeze(),y.squeeze(),temp[:,0])
+    # plt.colorbar(cs)
+    # plt.show()
+    
+    
+    
+    data =  get_boussineq_2D('Data/DHC_2D_Unsteady.dat',shuffle=False)
+    for i in range(0,100,5):
+        s0 = 240*i
+        s1 = 240*(i+1)
+        t = data[0:240,2]
+        u = data[s0:s1,3]
+        plt.plot(t,u)
+    plt.show()
+    
+
+
